ffd2f30673b4cb8562e01809a588b0df9d579bec
Yuvrajhere <yuvrajisbest13@gmail.com>
2020-08-16 20:19:42 +0530

    Updates

---
+++ precache-manifest.13242da8dd821d327d067ee98ddc12b5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78d0d05a289bc82a00b537b79c99db9",
+    "url": "/sorting-hat/index.html"
+  },
+  {
+    "revision": "9055daa0f51a55f8d55e",
+    "url": "/sorting-hat/static/css/main.90673b1e.chunk.css"
+  },
+  {
+    "revision": "04a2d385df1e1f26882a",
+    "url": "/sorting-hat/static/js/2.96293cc3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sorting-hat/static/js/2.96293cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9055daa0f51a55f8d55e",
+    "url": "/sorting-hat/static/js/main.18e4667a.chunk.js"
+  },
+  {
+    "revision": "0a774bc5d5113c9dde02",
+    "url": "/sorting-hat/static/js/runtime-main.6d4083a9.js"
+  },
+  {
+    "revision": "c54e5c28f73d0ca087f7c817d5efdeb0",
+    "url": "/sorting-hat/static/media/Albus-Dumbledore.c54e5c28.jpg"
+  },
+  {
+    "revision": "08b5b125ea02c6c604edce8502b4cce4",
+    "url": "/sorting-hat/static/media/Artemisia-Lufkin.08b5b125.png"
+  },
+  {
+    "revision": "ac0e148f41f367f96ee7ad92296361e5",
+    "url": "/sorting-hat/static/media/Draco-Malfoy.ac0e148f.png"
+  },
+  {
+    "revision": "e38406ba783a9673e4c70749720d37d7",
+    "url": "/sorting-hat/static/media/Gilderoy-Lockheart.e38406ba.png"
+  },
+  {
+    "revision": "73e299e8a90a53a49734b16360611c70",
+    "url": "/sorting-hat/static/media/Harry-Potter.73e299e8.jpg"
+  },
+  {
+    "revision": "ef7180e533ef370b6e0c866c1e92911a",
+    "url": "/sorting-hat/static/media/Hengist-Of-Woodcroft.ef7180e5.jpg"
+  },
+  {
+    "revision": "f488a907772a3107bbf7974684d8f9f0",
+    "url": "/sorting-hat/static/media/HogwartsCastleWallpapers-WallpaperCave.f488a907.jpg"
+  },
+  {
+    "revision": "ef483545e87697161853b2871be670c7",
+    "url": "/sorting-hat/static/media/Ignatia-Wildsmith.ef483545.png"
+  },
+  {
+    "revision": "b962b42432534f4e941dc926843f9a77",
+    "url": "/sorting-hat/static/media/Luna-Lovegood.b962b424.jpg"
+  },
+  {
+    "revision": "51f22f8c96a88186f8bd7af31c791721",
+    "url": "/sorting-hat/static/media/Merlin.51f22f8c.jpg"
+  },
+  {
+    "revision": "64cd452e65f6855771c2df761b0b99f5",
+    "url": "/sorting-hat/static/media/Minerva-McGonagall.64cd452e.jpg"
+  },
+  {
+    "revision": "1d7db99aaad01f9f662ce15f1ffba3ae",
+    "url": "/sorting-hat/static/media/Newt-Scamander.1d7db99a.png"
+  },
+  {
+    "revision": "63ce00e2752d219dc3eed10af07ada8e",
+    "url": "/sorting-hat/static/media/SimilarDesertedGerenuk-size_restricted.63ce00e2.gif"
+  },
+  {
+    "revision": "57e0b7d30e50d44ba6527fa4ae206089",
+    "url": "/sorting-hat/static/media/Tom-Riddle.57e0b7d3.jpg"
+  },
+  {
+    "revision": "05d6d134a1db4a61d5faefc03a8bec29",
+    "url": "/sorting-hat/static/media/dumbledore-protrait-miniature.05d6d134.jpg"
+  },
+  {
+    "revision": "613b3b9bedc4c736b9e611487614ba8c",
+    "url": "/sorting-hat/static/media/github.613b3b9b.png"
+  },
+  {
+    "revision": "7dd66c53f269cfa4a9a04625be51d922",
+    "url": "/sorting-hat/static/media/gryffindor.7dd66c53.png"
+  },
+  {
+    "revision": "d9272efa2b7d74743e3b00b3ce8b2af0",
+    "url": "/sorting-hat/static/media/hufflepuff.d9272efa.png"
+  },
+  {
+    "revision": "37b6c69a5dc98b83406b1422bb7cfc5f",
+    "url": "/sorting-hat/static/media/instagram.37b6c69a.png"
+  },
+  {
+    "revision": "5955922655eb449ad1f3179836416a6b",
+    "url": "/sorting-hat/static/media/ravenclaw.59559226.png"
+  },
+  {
+    "revision": "59f8fcf564f92740ac9e34ce8196535f",
+    "url": "/sorting-hat/static/media/slytherin.59f8fcf5.png"
+  },
+  {
+    "revision": "a01b160497667a5f27e0d4583d5a23a3",
+    "url": "/sorting-hat/static/media/twitter.a01b1604.png"
+  }
+]);
